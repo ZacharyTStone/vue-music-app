204eee473b2b4d56259058659df0ea661f8d2827
Zach Stone <zachinjapan888@gmail.com>
2022-06-15 12:30:52 -0400

    finish routing

--- src/store/index.js
+++ src/store/index.js
@@ -53,6 +53,10 @@ export default createStore({
       await auth.signOut();
 
       commit("toggleAuth");
+
+      // if (payload.route.meta.requiresAuth) {
+      //   payload.router.push({ name: 'home' });
+      // }
     },
   },
 });
